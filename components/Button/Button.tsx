92ec77913a60f0479f043686647eac529a9b245f
ваше имя <apel0491@gmail.com>
2022-05-13 23:38:38 +0300

    add edits

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,7 +5,10 @@ import cn from 'classnames';
 export const Button = ({appearance, children} : ButtonProps): JSX.Element => {
         return (
             <button
-                className={styles.button}
+                className={cn(styles.button, {
+                    [styles.primary]: appearance == 'primary',
+                    [styles.ghost]: appearance == 'ghost',
+                })}
             >
                 {children}
             </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Htag } from '../components'
+import { Button, Htag } from '../components'
 
 export default function Home() {
   return (
     <>
       <Htag tag='h1'>Текст</Htag>
+      <Button appearance='primary'>Кнопка</Button>
     </>
   )
 }
