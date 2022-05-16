1262cdbd6c5c067271e3349552a022f2cf5cc547
ваше имя <apel0491@gmail.com>
2022-05-16 21:15:20 +0300

    editing Paragraph

--- components/Paragraph/Paragraph.props.ts
+++ components/Paragraph/Paragraph.props.ts
@@ -1,5 +1,6 @@
-import { ReactNode } from "react";
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 
-export interface ParagraphProps {
+export interface ParagraphProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
+    size: 's' | 'm' | 'l';
     children: ReactNode;
 }
--- components/Paragraph/Paragraph.tsx
+++ components/Paragraph/Paragraph.tsx
@@ -1,6 +1,19 @@
 import { ParagraphProps } from "./Paragraph.props";
 import styles from './Paragragh.module.css';
+import cn from 'classnames';
 
-export const Paragragh = () => {
-    return 
+export const Paragragh = ({size = 'm', children, className, ...props}: ParagraphProps): JSX.Element => {
+    return (
+        <p
+            className={cn(styles.p, className, {
+                [styles.s]: size == 's',
+                [styles.m]: size == 'm',
+                [styles.l]: size == 'l', 
+            })}
+            {...props}
+        >
+            {children}
+        </p>
+        
+    );
 }
