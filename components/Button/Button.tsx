6654868e76885da6344bd2d5d3a718446fe79c91
ваше имя <apel0491@gmail.com>
2022-05-16 19:03:23 +0300

    fixing webpack

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
+import ArrowIcon from './arrow.svg';
 import cn from 'classnames';
 
 export const Button = ({appearance, arrow = 'none', children, className, ...props} : ButtonProps): JSX.Element => {
@@ -15,7 +16,7 @@ export const Button = ({appearance, arrow = 'none', children, className, ...prop
                 {arrow != 'none' && <span className={cn(styles.arrow, {
                     [styles.down]: arrow = 'down'
                 })}>
-                    <img src="./arrow.svg" alt=""/>
+                    <ArrowIcon />
                     </span>}
             </button>
         );
