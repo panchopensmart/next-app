b7014d9b3adee7c2142eaf6c2b8ee454852821f3
ваше имя <apel0491@gmail.com>
2022-05-20 21:10:05 +0300

    fixing webpack

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,9 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-export declare module "*.svg" {
-    const content: any;
-    export default content;
-}
-
